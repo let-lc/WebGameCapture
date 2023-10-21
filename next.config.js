5b52399d744d168e41a3aea7b9d51bee950d07cd
Liuming Chen <51524534+let-lc@users.noreply.github.com>
2023-10-21 00:22:09 -0400

    ci: add github pages deployment workflow and configurations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/WebGameCapture',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
