53ba775297123ee9f474d847988561e8140e80ff
Liuming Chen <51524534+let-lc@users.noreply.github.com>
2023-10-22 01:26:12 -0400

    refactor: ui show up logics and volume control not sync issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,46 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
 import dynamic from 'next/dynamic';
 
-import { useDebounce, useMouse } from 'react-use';
-
-import { useVolumeStore } from '@/hooks/useVolumeStore';
+import { useDebounce } from 'react-use';
 
 const Menu = dynamic(() => import('@/components/app/Menu'), { loading: () => null });
 const ScreenshotButton = dynamic(() => import('@/components/app/ScreenshotButton'), { loading: () => null });
 const FullscreenButton = dynamic(() => import('@/components/app/FullscreenButton'), { loading: () => null });
 const PiPButton = dynamic(() => import('@/components/app/PictureInPictureButton'), { loading: () => null });
-const VolumeControl = dynamic(() => import('@/components/app/VolumeControl'), { loading: () => null });
+const VolumeControl = dynamic(() => import('@/components/app/VolumeControl'), { loading: () => null, ssr: false });
 const Video = dynamic(() => import('@/components/app/Video'), { loading: () => null });
 
 const HomePage = () => {
-  const ref = useRef(null);
-  const { docX, docY } = useMouse(ref);
-  const volume = useVolumeStore((s) => s.volume);
-  const [showUI, setShowUI] = useState(true);
+  const [showLeft, setShowLeft] = useState(true);
+  const [showRight, setShowRight] = useState(true);
 
   useDebounce(
     () => {
-      setShowUI(false);
+      setShowLeft(false);
+      setShowRight(false);
     },
     2000,
-    [docX, docY, volume]
+    [showLeft, showRight]
   );
 
-  useEffect(() => {
-    if (!showUI) {
-      setShowUI(true);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [docX, docY, volume]);
-
   return (
-    <div ref={ref}>
+    <div>
       <Video />
+      <div onMouseOver={() => setShowLeft(true)} className="peer fixed inset-y-0 left-0 w-16" />
       <div
-        className="fixed inset-y-0 left-0 flex -translate-x-full flex-col justify-center gap-2 p-4 opacity-0 transition-all duration-300 data-[show=true]:translate-x-0 data-[show=true]:opacity-100"
-        data-show={String(showUI)}
+        className="fixed inset-y-0 left-0 flex -translate-x-full flex-col justify-center gap-2 p-4 opacity-0 transition-all duration-300 hover:translate-x-0 hover:opacity-100 data-[show=true]:translate-x-0 data-[show=true]:opacity-100"
+        data-show={String(showLeft)}
       >
         <Menu />
         <FullscreenButton />
         <PiPButton />
         <ScreenshotButton />
       </div>
+      <div onMouseOver={() => setShowRight(true)} className="peer fixed inset-y-0 right-0 w-16" />
       <div
-        className="fixed inset-y-0 right-0 flex translate-x-full flex-col justify-center gap-2 p-4 opacity-0 transition-all duration-300 data-[show=true]:translate-x-0 data-[show=true]:opacity-100"
-        data-show={String(showUI)}
+        className="fixed inset-y-0 right-0 flex translate-x-full flex-col justify-center gap-2 p-4 opacity-0 transition-all duration-300 hover:translate-x-0 hover:opacity-100 data-[show=true]:translate-x-0 data-[show=true]:opacity-100"
+        data-show={String(showRight)}
       >
         <VolumeControl />
       </div>
